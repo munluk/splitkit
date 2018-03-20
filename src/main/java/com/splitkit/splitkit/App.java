cd2573c58434e7d864c88becbf2a98cc7905b5f4
Lukas Munteanu <lukas.munteanu.nr1@googlemail.com>
2018-03-20 18:44:17 +0100

    Added project

---
+++ src/main/java/com/splitkit/splitkit/App.java
@@ -0,0 +1,14 @@
+package com.splitkit.splitkit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World the 2nd!" );
+    }
+}
